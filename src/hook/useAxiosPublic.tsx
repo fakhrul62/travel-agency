ac97a3aec431531d6332bc768ec394a62b8998b9
fakhrul62 <ifakhrul23@gmail.com>
2025-06-21 14:03:17 +0600

    vercel api fixed

--- src/hook/useAxiosPublic.tsx
+++ src/hook/useAxiosPublic.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "https://travel-agency-server-delta.vercel.app//",
+    baseURL: "https://travel-agency-server-delta.vercel.app/",
 })
 
 const useAxiosPublic = () => {
--- src/lib/axiosSecure.ts
+++ src/lib/axiosSecure.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "https://travel-agency-server-delta.vercel.app//",
+  baseURL: "https://travel-agency-server-delta.vercel.app/",
 });
 
 // Attach token if available
